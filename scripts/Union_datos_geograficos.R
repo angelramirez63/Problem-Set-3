4482b09ba5029489133eb09a20bd3b441003686a
platangel <n.plata@uniandes.edu.co>
2025-05-07 23:08:46 -0500

    Variables espaciales
    estaciones de transmi, sitp, avaluo por manzana, estrato

--- scripts/Union_datos_geograficos.R
+++ scripts/Union_datos_geograficos.R
@@ -78,67 +78,24 @@ distneastfeat<-function(data_original, data_feat, n_variable, tipo_dato){
   return(data_original)
 }
 
-#Para traer más variables
-distneastfeatvar<- function(data_original, data_feat, 
-                          n_variable_distancia, 
-                          tipo_dato = "punto", 
-                          variables_adicionales = NULL,
-                          sufijo_adicional = "_cercano") {
+#Funcion para distancia minima
+distminpoints<-function(data_orginial,ext_points){
   
-  # Calcular centroides si data_feat es polígono
-  if (tipo_dato == "poligono") {
-    data_feat <- st_centroid(data_feat, byid = TRUE)
-  }
+  minimos<-c()
   
-  # Calcular matriz de distancias
-  dist_matrix <- st_distance(data_original, data_feat)
-  
-  # Obtener distancia mínima y su posición
-  dist_min <- apply(dist_matrix, 1, min)
-  idxs_mas_cercanos <- apply(dist_matrix, 1, which.min)
-  
-  # Asignar distancia mínima
-  data_original[[n_variable_distancia]] <- dist_min
-  
-  # Si se piden variables adicionales, asignarlas también
-  if (!is.null(variables_adicionales)) {
-    if (!all(variables_adicionales %in% names(data_feat))) {
-      stop("Alguna de las variables adicionales no está en 'data_feat'.")
-    }
+  for (i in 1:nrow(data_orginial)){
     
-    for (var in variables_adicionales) {
-      nuevo_nombre <- paste0(var, sufijo_adicional)
-      data_original[[nuevo_nombre]] <- data_feat[[var]][idxs_mas_cercanos]
-    }
+    vector_dist<-st_distance(data_orginial[i,],ext_points)
+    min<-min(vector_dist)
+    
+    minimos<-c(minimos,min)
+    print(minimos)
+    
+    rm(vector_dist)
+    
   }
   
-  return(data_original)
-}
-
-#Halla distancias de forma iterativa - esta ocupa menos RAM
-nearestpointloop <- function(base_objetivo, base_fuente, variables_fuente, sufijo = "_cercano") {
-  # Validaciones
-  if (!inherits(base_objetivo, "sf") || !inherits(base_fuente, "sf")) {
-    stop("Ambas bases deben ser objetos 'sf'.")
-  }
-  
-  if (!all(variables_fuente %in% names(base_fuente))) {
-    stop("Algunas variables no existen en la base fuente.")
-  }
-  
-  # Crear matriz de distancias (filas = base_objetivo, columnas = base_fuente)
-  distancias <- st_distance(base_objetivo, base_fuente)  # matriz [nrow(objetivo) x nrow(fuente)]
-  
-  # Para cada fila de base_objetivo, encontrar el índice del punto más cercano en base_fuente
-  idxs_mas_cercanos <- apply(distancias, 1, which.min)
-  
-  # Para cada variable solicitada, pegarla desde el punto más cercano
-  for (var in variables_fuente) {
-    nuevo_nombre <- paste0(var, sufijo)
-    base_objetivo[[nuevo_nombre]] <- base_fuente[[var]][idxs_mas_cercanos]
-  }
-  
-  return(base_objetivo)
+  return(minimos)
 }
 
 # Cargar datos -----------------------------------------------------------------
@@ -176,10 +133,26 @@ bibliotecas <- st_read("redbibliotecacademica/RedBibliotecAcademica.shp")
 museo <- st_read("museos/Museo.shp")
 colegios <- st_read("Colegios/Colegios03_2024.shp")
 recaudo_predial <- st_read("rpredial/RPREDIAL.shp")
+manzanas_estr<-st_read("manz_estrato/ManzanaEstratificacion.shp")
+sitp<- st_read("psitp/PSITP.shp")
+delitos<-st_read("dai/DAILoc.shp")
+restbar<-st_read("egba/EGBA.shp")
+cicloruta<-st_read("ciclorruta/ciclorruta.shp")
+manzanas_aval<-st_read("avaluo_manz/Avaluo_Manzana.shp")
+manzanas_area_res <- st_read("AconstruidaResidencialSHP/AconstruidaResidencial.shp")
+manzanas_area_com <- st_read("AconstruidaComercialSHP/AconstruidaComercial.shp")
+
+# Estaciones de transmilenio
+transmi <- st_read('https://gis.transmilenio.gov.co/arcgis/services/Troncal/consulta_estaciones_troncales/MapServer/WFSServer?request=GetCapabilities&service=WFS')
+transmi<- as.data.frame(transmi) %>% dplyr::select(-Shape)
+transmi <- st_as_sf(transmi, coords = c("LONGITUD", "LATITUD"),
+                    crs = 4326) %>% st_make_valid()
 
 #Homogenizar datos geográficos
 datasets <- c("colegios", "indicador_loc", "indicador_upz", "luminarias_upz",
-              "seguridad_nocturna", "bibliotecas", "museo","recaudo_predial")
+              "seguridad_nocturna", "bibliotecas", "museo","recaudo_predial", 
+              "manzanas_estr", "sitp", "delitos", "restbar", "cicloruta", 
+              "manzanas_aval")
 
 for (data_name in datasets) {
   # Obtener la base de datos por su nombre
@@ -222,12 +195,76 @@ datos <- distneastfeat(datos, colegios, 'distnearestschool', 'puntos')
 #Museos
 datos <- distneastfeat(datos, museo, 'distnearestmuseum', 'puntos')
 
+#Transmi
+datos <- distneastfeat(datos, transmi, 'distnearesttransmi', 'puntos')
+
+
 #Recaudo predial
 recaudo_predial <- recaudo_predial %>% 
   select(geometry, Sum_VALOR_) %>%
   rename(recaudo_predial = Sum_VALOR_)
 
 datos <- st_join(datos, recaudo_predial, join = st_within)
+
+#Estrato
+manzanas_estr <- manzanas_estr %>% 
+  select(geometry, ESTRATO)
+sf_use_s2(FALSE) ### Linea necesaria pára correr el codigo
+st_nearest_feature(datos, manzanas_estr)
+datos <- st_join(datos, manzanas_estr, join = st_nearest_feature)
+
+#Avaluo manzana
+manzanas_aval <- manzanas_aval %>% 
+  select(geometry, AVALUO_COM, AVALUO_CAT, GRUPOP_TER) %>%
+  rename(aval_comer_manz = AVALUO_COM, aval_catas_manz = AVALUO_CAT,
+         grupo_econom_manz = GRUPOP_TER)
+sf_use_s2(FALSE) ### Linea necesaria pára correr el codigo
+st_nearest_feature(datos, manzanas_aval)
+datos <- st_join(datos, manzanas_aval, join = st_nearest_feature)
+
+#SITP
+datos <-distneastfeat(datos, sitp, 'distnearestsitp','punto')
+
+#Delitos
+remove <- grep("TOT|VAR|22|18|24|23", names(delitos))
+delitos <- delitos[, -remove]
+
+#Renombrar variables
+nombres_anteriores <- c('CMLP', 'CMHP', 'CMHR', 'CMHA', 'CMHB', 'CMHCE', 'CMHM', 'CMDS', 'CMVI')
+reemplazos <- c("lesionesperson", "hurtopersonas", 'hurtosresidencias', 'hurtosautos', 'hurtosbicis',  'hurtoscel', 'hurtosmotos', 'delitos_sexual', 'violencia_intra')
+nombres_n <- str_replace_all(names(delitos), setNames(reemplazos, nombres_anteriores))
+nombres_n <- gsub("CMHC", "hurtoscomercio", nombres_n) 
+nombres_n <- gsub("CMH", "homicidios", nombres_n)   
+colnames(delitos)<-nombres_n
+
+#Promedio de delitos para los año de venta de  las proepiedades 2019, 2020, 2022as
+delitos<-as.data.frame(delitos)
+
+delitos <- delitos %>%
+  mutate(n_homicidios = rowMeans(dplyr::select(., starts_with("homicidios"))),
+         n_lesiones = rowMeans(dplyr::select(., starts_with("lesionesperson"))),
+         n_hurtopersonas = rowMeans(dplyr::select(., starts_with("hurtopersonas"))),
+         n_hurtosautos=rowMeans(dplyr::select(., starts_with("hurtosautos"))),
+         n_hurtosresidencias=rowMeans(dplyr::select(., starts_with("hurtosresidencias"))),
+         n_hurtosbicis=rowMeans(dplyr::select(., starts_with("hurtosbicis"))),
+         n_hurtosmotos=rowMeans(dplyr::select(., starts_with("hurtosmotos"))),
+         n_hurtoscomercio=rowMeans(dplyr::select(., starts_with("hurtoscomercio"))),
+         n_hurtoscelular=rowMeans(dplyr::select(., starts_with("hurtoscel"))),
+         n_delitossexales=rowMeans(dplyr::select(., starts_with("delitos_sexual"))),
+         n_violenciaintra=rowMeans(dplyr::select(., starts_with("violencia_intra"))))
+
+# Nos quedamos con los promedios e identificador (localidad) y Quitamos las tildes a la variable localidad
+delitos <-delitos %>% rename('n_localidad'='CMIULOCAL','localidad'='CMNOMLOCAL')  %>%
+  dplyr::select(localidad,starts_with("n_"))  %>% 
+  mutate(localidad=chartr("áéíóúÁÉÍÓÚ", "aeiouAEIOU", localidad)) %>%
+  mutate(localidad=tolower(localidad))
+
+# Unimos los delitos promedio a los datos
+datos <- datos %>% rename('localidad'='LocNombre')  %>%
+  mutate(localidad=tolower(localidad)) # Cambiamos nombre de localidad para pegar variables
+datos <- left_join(x=datos, y=delitos, by='localidad')
+
+
 
 ## Datos OpenStreetMap ---------------------------------------------------------
 
